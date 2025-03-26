4154c8a06653437cb901a98bea245a36e95d3f85
João Vitor <002.joaovitor@gmail.com>
2025-03-26 16:47:43 -0300

    segundoCommit

---
+++ Banco de Dados de Login.sql
@@ -0,0 +1,39 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.4.32-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.8.0.6908
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para bancologin
+CREATE DATABASE IF NOT EXISTS `bancologin` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `bancologin`;
+
+-- Copiando estrutura para tabela bancologin.login
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(100) DEFAULT NULL,
+  `senha` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela bancologin.login: ~2 rows (aproximadamente)
+INSERT INTO `login` (`id`, `usuario`, `senha`) VALUES
+	(1, 'user1', '123456'),
+	(2, 'user2', '654321');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
--- app.js
+++ app.js
@@ -6,12 +6,23 @@
 // Importar o modulo para o projeto
 const express = require("express")
 const path = require("path")
+const mysql2 = require("mysql2/promise")
+const { error } = require("console")
+
+// Conexão com o banco
+const connection = mysql2.createPool({
+    host: 'localhost',
+    user: 'root', 
+    password: '', 
+    database: 'bancologin' 
+})
 
 
 // Configura o express para ser usado
 const App = express()
 App.set("view engine", "ejs")
-
+App.use(express.urlencoded({extended: true}))
+App.use(express.json())
 // Servindo arquivos estáticos (CSS, imagens, etc.) na pasta mvc depois views
 App.set("views", path.join(__dirname, "mvc/views"))
 App.use(express.static(path.join(__dirname, "public")))
@@ -21,6 +32,37 @@ App.use(express.static(path.join(__dirname, "public")))
 App.get("/", (req,res) => {
     res.render("index.ejs")
 })
+
+App.post("/login", async (req,res) => {
+
+    let usuario = ""
+    let senha = ""
+
+    if(req.body.usuario === ""  || req.body.senha === ""){
+        res.render("error.ejs")
+    }
+
+
+    usuario = req.body.usuario
+    senha = req.body.senha
+
+
+
+    const sql = "SELECT * FROM login WHERE usuario = ? AND senha = ?"
+
+    const [rows] = await connection.execute(sql, [usuario,senha])
+    
+    if(rows.length === 0) {
+        res.render("error", {
+            mensagem: "Usuário ou senha inválidos!"
+        })
+    }
+    
+    // Se o login for bem-sucedido, renderiza a página 'conta.ejs'  
+    res.render("conta.ejs");
+
+})
+
 
 
 // Ligar/deixar disponivel o serviço
